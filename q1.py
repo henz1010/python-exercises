e5482a03f6622bb59dd1f451948a5e3fef9dc6f2
hezagon <hezagon99@gmail.com>
2021-08-22 16:57:10 +0800

    Add q1

---
+++ q1.py
@@ -0,0 +1,16 @@
+# Python program to add two numbers
+
+# Given two numbers, num1 and num2. The task is to write a Python program to find the addition of these two numbers.
+
+# OUTPUT:
+# Input:
+# num1 = 5
+# num2 = 3
+# Output: 8
+
+num1 = input("Input: \nnum1 = ")
+num2 = input("num2 = ")
+
+sum = int(num1) + int(num2)
+
+print('Output:', sum)
